e3564cdabcffe72110a579b88a15f357989fed0c
Chandler <chandler.vanscoy@gmail.com>
2016-12-11 16:41:59 -0800

    Added Character Controller, Mouse Look, and Firearm
    The character controller, mouse look, and basic firearm script have been
    implemented.

---
+++ Assets/Scripts/Placeholders/BasicFirearm.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BasicFirearm : MonoBehaviour
+{
+    public float fireRate = 0.3F;
+    public float damage = 10;
+    public AudioClip weaponSound;
+
+    float timeToNextShot = 0;
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(Time.time >= timeToNextShot && Input.GetButton("Fire1"))
+        {
+            audioSource.PlayOneShot(weaponSound, 1.0F);
+            timeToNextShot = Time.time + fireRate;
+        }
+    }
+}
